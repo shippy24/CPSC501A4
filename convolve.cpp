031ea793e9d9b45ede5a3f33327403ece0127ea4
Shayne Mujuru <shaynemujuru@gmail.com>
2019-04-12 01:44:55 -0600

    Initial commit and initializations

---
+++ convolve.cpp
@@ -0,0 +1,7 @@
+/**
+  * NAME: Shayne Mujuru
+  * UCID: 30029552
+  * PROJ: AIV Code Optimization
+  * Convolution of audio files using Smith Textbook Input Side Algorithm
+  * Base code taken from Dr Leonard Menzara
+  */
